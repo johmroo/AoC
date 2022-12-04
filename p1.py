7f6503cc8dfcbdab7414d2e09543c04cfb1745ab
JOhan Roos <johroo@gmail.com>
2022-12-04 10:58:47 +0100

    day1

---
+++ p1.py
@@ -0,0 +1,16 @@
+
+def p1_1(f):
+    l = f.read().split("\n\n")
+    q = [sum(int(i) for i in x.split()) for x in l]
+    return max(q)
+
+def p1_2(f):
+    l = f.read().split("\n\n")
+    q = [sum(int(i) for i in x.split()) for x in l]
+    q.sort()
+    return sum(q[-3:])
+
+f = open('data1', 'r')
+print(p1_1(f))
+f = open('data1', 'r')
+print(p1_2(f)) 

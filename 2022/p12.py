50111ee8d177c0e293825455fc72b56ecdd4d475
Johan <46325263+johmroo@users.noreply.github.com>
2022-12-12 07:39:18 +0100

    day12

--- 2022/p12.py
+++ 2022/p12.py
@@ -12,8 +12,7 @@ def elev(i,j):
         return ord('a')
     else:
         return ord(m[i][j])
-
-
+    
 def solve(n):
     Q = deque()
     for i in range(rows):
@@ -24,15 +23,14 @@ def solve(n):
     S = set()
     while Q:
         (i,j),c = Q.popleft()
+        if m[i][j]=='E':
+            return c
         if (i,j) in S:
             continue
         S.add((i,j))
-        if m[i][j]=='E':
-            return c
-        for a,b in [(-1,0),(1,0),(0,-1),(0,1)]:
-            x,y = i+a,j+b
+        for x,y in [(i-1,j),(i+1,j),(i,j-1),(i,j+1)]:
             if 0<=x<rows and 0<=y<cols and elev(x,y)<=1+elev(i,j):
                 Q.append(((x,y),c+1))
-r = solve(0)
-print(r)
-print(min(solve(1),r))
+
+print(solve(0))
+print(solve(1))

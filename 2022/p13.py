6c4d9384d35d89bdfb11addb6c190b497c29fc51
Johan <46325263+johmroo@users.noreply.github.com>
2022-12-13 08:44:16 +0100

    day13

--- 2022/p13.py
+++ 2022/p13.py
@@ -1,20 +1,16 @@
-def cmp(x, y):
+def cmp(x,y):
     if type(x) == int:
         if type(y) == int: return x - y
         else: return cmp([x], y)
-    else: 
-        if type(y) == int: return cmp(x, [y])    
-    for a, b in zip(x, y):
-        v = cmp(a, b)
+    elif type(y) == int: return cmp(x, [y])    
+    for a,b in zip(x, y):
+        v = cmp(a,b)
         if v: return v
     return len(x) - len(y)
 
 f = open("2022/day13.txt")
 p = [[eval(x) for x in cc.splitlines()] for cc in f.read().split("\n\n")]
-c = 0
-for i, (a, b) in enumerate(p):
-    if cmp(a, b) < 0: c += i + 1
-print(c)
+print(sum([i+1 for i, (a, b) in enumerate(p) if cmp(a, b) < 0]))
 
 f = open("2022/day13.txt")
 p = [[eval(x) for x in cc.splitlines()] for cc in f.read().split()] + [[[2]], [[6]]]
@@ -24,4 +20,4 @@ for a in p:
         d1 += 1
         d2 += 1
     elif cmp(a, [[6]]) < 0: d1 += 1
-print(d1 * d2)
+print(d1*d2)

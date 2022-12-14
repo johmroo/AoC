65d447e3c41bbd03853f667fd8c62fb1d5211a3b
Johan <46325263+johmroo@users.noreply.github.com>
2022-12-14 08:00:29 +0100

    day14

--- 2022/p14.py
+++ 2022/p14.py
@@ -1,35 +1,30 @@
 def solve(c,b,n):
     t = 0
-    while True:
+    while (500,0) not in c:
         x,y = 500,0
         while True:
+            if y>=b: break
             if (x,y+1) not in c: y = y+1
             elif (x-1,y+1) not in c: x,y = x-1,y+1
             elif (x+1,y+1) not in c: x,y = x+1,y+1
             else: break  
         t += 1
         if y+1>=b and n == 0: return t-1
-        if x == 500 and y == 0: return t
         c.add((x,y))
+    return t
 
-f = open("2022/day14.txt").read().strip().split('\n')
+f = open("2022/day14.txt")
 cave = set()
-for i in f:
-    last = None
-    for j in i.split('->'):
-        x,y = j.split(',')
-        x,y = int(x),int(y)
-        if last is not None:
-            nx,ny = x-last[0],y-last[1]
-            l = max(abs(nx),abs(ny))
-            dx = 1 if nx>0 else (-1 if nx<0 else 0)
-            dy = 1 if ny>0 else (-1 if ny<0 else 0)
-            for i in range(l+1): cave.add((last[0]+i*dx,last[1]+i*dy))
-        last = (x,y)
+for line in open("2022/day14.txt"):
+    x = [list(map(int, p.split(","))) for p in line.split(" -> ")]
+    for (x1, y1), (x2, y2) in zip(x, x[1:]):
+        x1, x2 = sorted([x1, x2])
+        y1, y2 = sorted([y1, y2])
+        for x in range(x1, x2 + 1):
+            for y in range(y1, y2 + 1):
+                cave.add((x,y))
 
-cave_floor = 2+max(x[1] for x in cave)
-minx,maxx = min(x[0] for x in cave)-500,max(x[0] for x in cave)+500
-for i in range(minx, maxx): cave.add((i,cave_floor))
+cave_floor = 1+max(x[1] for x in cave)
 cave2 = cave.copy()
 print(solve(cave,cave_floor,0))
 print(solve(cave2,cave_floor,1))
